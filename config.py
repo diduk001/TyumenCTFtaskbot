41743a619516ad4c670b9a23075c4056a5c8674d
ToxicVasiliy <44618834+ToxicVasiliy@users.noreply.github.com>
2021-08-17 22:19:18 +0500

    Какая то кибер регулярка и кибер константы
    Кибер комит

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
+import re
 from sqlalchemy import create_engine
 
 class Config:
+    MAIL_REGEX = re.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
     # Bot Token
     try:
-        BOT_TOKEN = os.environ["BOT_TOKEN"]
+        BOT_TOKEN = "1955074488:AAFYCedNi4Frw4gwNvh_dCQ0R_HzXiSa1_E"
     except KeyError:
         raise KeyError("Envionment variable BOT_TOKEN is not defined")
 
     # DATABASE = "mongodb:///?Server=127.0.0.1&;Port=27017&Database=bot_db"
-    # engine = create_engine(DATABASE)
+    # engine = create_engine(DAT    ABASE)
