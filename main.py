c3d06ca782b9566d88f4d977310799dedd6cde23
ToxicVasiliy <44618834+ToxicVasiliy@users.noreply.github.com>
2021-08-18 00:55:26 +0500

    Сделал гейскую регистрацию

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from resources import Resources
 from config import Config
-from sqlalchemy import create_engine
 import re
 from aiogram import Bot, Dispatcher, executor, types
-from models import User
 
 bot = Bot(Config.BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -22,7 +20,7 @@ school = ''
 registration_complete = ''
 
 
-@dp.message_handler(commands='start')  # Старт регистрации
+@dp.message_handler(commands='start')
 async def start_cmd_handler(message: types.Message):
     global stage
     await message.answer(Resources.START_MSG)
@@ -30,84 +28,41 @@ async def start_cmd_handler(message: types.Message):
     stage += 1
 
 
-@dp.message_handler()  # Регистрация имени
+@dp.message_handler()
 async def name_handler(message: types.Message):
-    global stage
-    global user_name
+    global stage, user_name, user_surname, mail, mail_valid, nickname, city, age, grade, school, registration_complete
     if stage == 1:
         user_name = message.text
-    await message.answer(Resources.SURNAME)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация фамилии
-async def surname_handler(message: types.Message):
-    global stage
-    global user_surname
-    if stage == 2:
+        await message.answer(Resources.SURNAME)
+        stage += 1
+    elif stage == 2:
         user_surname = message.text
-    await message.answer(Resources.MAIL)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация мыла
-async def mail_handler(message: types.Message):
-    global stage
-    global mail
-    if stage == 3:
+        await message.answer(Resources.MAIL)
+        stage += 1
+    elif stage == 3:
         mail = message.text
-    await message.answer(Resources.NICKNAME)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация ника
-async def nickname_handler(message: types.Message):
-    global stage
-    global nickname
-    if stage == 4:
+        await message.answer(Resources.NICKNAME)
+        stage += 1
+    elif stage == 4:
         nickname = message.text
-    await message.answer(Resources.CITY)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация города
-async def city_handler(message: types.Message):
-    global stage
-    global city
-    if stage == 5:
+        await message.answer(Resources.CITY)
+        stage += 1
+    elif stage == 5:
         city = message.text
-    await message.answer(Resources.AGE)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация возраста
-async def age_handler(message: types.Message):
-    global stage
-    global age
-    if stage == 6:
+        await message.answer(Resources.AGE)
+        stage += 1
+    elif stage == 6:
         age = message.text
-    await message.answer(Resources.GRADE)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация класса
-async def grade_handler(message: types.Message):
-    global stage
-    global grade
-    if stage == 7:
+        await message.answer(Resources.GRADE)
+        stage += 1
+    elif stage == 7:
         grade = message.text
-    await message.answer(Resources.SCHOOL)
-    stage += 1
-
-
-@dp.message_handler()  # Регистрация школы
-async def school_handler(message: types.Message):
-    global stage
-    global school
-    if stage == 7:
+        await message.answer(Resources.SCHOOL)
+        stage += 1
+    elif stage == 8:
         school = message.text
-    await message.answer(Resources.REGISTRATION_COMPLETE)
-    school += 1
+        await message.answer(Resources.REGISTRATION_COMPLETE)
+        stage += 1
 
 
 if __name__ == '__main__':
